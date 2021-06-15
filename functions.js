5c8cd17f99579e9abe493e9cd54e70ac718d3734
lucianodovidio <luchofx250@gmail.com>
2021-06-15 16:12:47 -0300

    Se realizo animacion y modifico js

--- functions.js
+++ functions.js
@@ -3,32 +3,58 @@
  * @method ampliarR
  * @param {string} id - El id de los inputs cantcil,cc.
  * @param ´{number} valor - El valor de los inputs de cantcil, cc.
- * @return Valor que retorna
  */
 
 
-function ampliarR(id, valor){
+function ampliarR(){
 
-    var cantcilindros, cilindrada;
-    var canvas = document.getElementById("myCanvas");
+    var cantcilindros, cilindrada, resultado;
 
     cantcilindros = document.getElementsByName("cantcilindros") [0].value;
     cilindrada = document.getElementsByName("cilindrada") [0].value;
 
-    if(isNaN(valor)){
-        alert("Se ingreso un valor invalido");
-        cantcilindros = "";
-        cilindrada = "";
+    if(isNaN(cantcilindros)){
+        alert("Se ingreso un valor invalido en Cantidad de Cilindros");
+        document.getElementsByName("cantcilindros")[0].value = "";
+    }else if(isNaN(cilindrada)){
+        alert("Se ingreso un valor invalido en Cilindrada")
+        document.getElementsByName("cilindrada")[0].value = "";
     }else{
-        document.getElementsByName("resultadoo") [0].value = ((cilindrada / cantcilindros) + cantcilindros * 10) / cantcilindros * 10;
+        resultado = ((cilindrada / cantcilindros) + cantcilindros * 10) / (cantcilindros * 10);
+        document.getElementsByName("resultadoo")[0].value = resultado;
+    }
+
+}
+
+
+y=0;
+dy=2;
+function animarPiston(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width=canvas.width;
+
+
+    var img = new Image();
+    img.src = "MediaFiles/pistondiagrama.png";
+    img.onload = function(){
+        ctx.drawImage(img, 0, y);
     }
 
 
-    var ctx = canvas.getContext("2d");
-    var img = new Image();
-    img.src = "MediaFiles/pistondiagrama.png";
+    var img2 = new Image();
+    img2.src = "MediaFiles/camisa.png";
+    img2.onload = function(){
+        ctx.drawImage(img2, 0, 15);
+    }
 
-    ctx.drawImage(img, 0, 0);
 
+
+    if(y>120){
+        y=0;
+    }
+
+    y+=dy;
 }
 
