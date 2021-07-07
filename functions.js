383f65c34b40e8c1001060d5480d6f1f72b6af43
lucianodovidio <luchofx250@gmail.com>
2021-07-07 10:53:05 -0300

    Se realizaron correciones para el FINAL

--- functions.js
+++ functions.js
@@ -1,11 +1,8 @@
 /**
- * Calculadora de Compresion de un motor.
- * @method ampliarR
- * @param {string} id - El id de los inputs cantcil,cc.
- * @param {number} valor - El valor de los inputs de cantcil, cc.
+ * Calculadora de Relacion de Compresion
+ * @method ampliaR
  * @return resultado
  */
-
 function ampliarR(){
 
     var cantcilindros, cilindrada, resultado;
@@ -24,9 +21,15 @@ function ampliarR(){
         document.getElementsByName("resultadoo")[0].value = resultado;
         return resultado;
     }
+
+
 }
 
 
+/**
+ * Animacion del piston
+ * @method animarPiston
+ */
 y=0;
 dy=2;
 function animarPiston(){
@@ -34,32 +37,49 @@ function animarPiston(){
     var ctx = canvas.getContext("2d");
     var resultado = ampliarR();
     var decimal = resultado.toFixed(2);
+    var cantcilindros, cilindrada;
+
+    cantcilindros = document.getElementsByName("cantcilindros") [0].value;
+    cilindrada = document.getElementsByName("cilindrada") [0].value;
+
+
+
     canvas.width=canvas.width;
 
+    if(cantcilindros!="" && cilindrada!=""){
+        var img = new Image();
+        img.src = "MediaFiles/pistondiagrama.png";
+        img.onload = function(){
+            ctx.drawImage(img, 0, y);
+        }
 
-    var img = new Image();
-    img.src = "MediaFiles/pistondiagrama.png";
-    img.onload = function(){
-        ctx.drawImage(img, 0, y);
+
+        var img2 = new Image();
+        img2.src = "MediaFiles/camisa.png";
+        img2.onload = function(){
+            ctx.drawImage(img2, 0, 15);
+        }
+
+        var t=5;
+
+        if(y>120){
+            y=0;
+        }
+
+        y+=dy;
+
+        ctx.font = "italic small-caps bold 20px arial";
+        ctx.fillStyle= "white";
+        ctx.fillText(decimal + " : 1",250,55);
     }
 
 
-    var img2 = new Image();
-    img2.src = "MediaFiles/camisa.png";
-    img2.onload = function(){
-        ctx.drawImage(img2, 0, 15);
-    }
-
-    var t=5;
-
-    if(y>120){
-        y=0;
-    }
-
-    y+=dy;
-
-    ctx.font = "italic small-caps bold 20px arial";
-    ctx.fillStyle= "white";
-    ctx.fillText(decimal + " : 1",250,55);
 }
 
+/**
+ * Boton para refrescar la pagina
+ * @method refreshP
+ */
+function refreshP(){
+    location.reload();
+}
